144bb91f02cd36ba1225811694fa32d4e8cfea45
taterntots <matthew.a.weidner@gmail.com>
2019-11-18 16:38:20 -0500

    created a PrivateRoute component to take a user to their recipe upon authentification of a token. Cannot test until backend is deployed.

--- recipe-creator/src/App.js
+++ recipe-creator/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import Login from './components/Login';
 import Registration from './components/Registration';
+import RecipeList from './components/RecipeList';
+import PrivateRoute from './components/PrivateRoute';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
@@ -10,6 +12,7 @@ function App() {
     <Router>
       <main>
         <NavBar />
+        <PrivateRoute exact path='recipes' component={RecipeList} />
         <Route path='/login' component={Login} />
         <Route path='/register' component={Registration} />
       </main>
---
+++ recipe-creator/src/components/RecipeList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RecipeList() {
+    return (
+        <h1>TestList</h1>
+    )
+}
+
+export default RecipeList;
